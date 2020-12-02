c54be81c5856772810640e0db64b867c71877579
Stewart A <strangelytyped+github-23f9bb@gmail.com>
2020-12-02 21:36:21 +0000

    Day 2 and add tests

--- 1.js
+++ 1.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+
 const input = `1755
 1668
 837
@@ -197,18 +199,30 @@ const input = `1755
 1640
 1479
 1912
-1638`;
+1638`
 
-let part1_1 = input.split("\n").map(i => parseInt(i)).filter((i, idx, arr) => arr.includes(2020 - i)).map(i => i * (2020 - i))[0];
+const testInput = `1721
+979
+366
+299
+675
+1456`.split("\n").map(i => parseInt(i));
+
+let inputLines = input.split("\n").map(i => parseInt(i));
+
+let attempt1 = (input) => input.filter((i, idx, arr) => arr.includes(2020 - i)).map(i => i * (2020 - i))[0];
+
+assert.equal(attempt1(testInput), 514579)
+let part1_1 = attempt1(inputLines);
 console.log("Part 1 attempt 1", part1_1)
 
-function findStuff(targetVal, itemCount, arr){
+function attempt2(targetVal, itemCount, arr){
     for(let i = 0; i < arr.length; i++){
         if(arr[i] == targetVal && itemCount == 1){
             return [arr[i]]
         }
         if(arr[i] < targetVal && itemCount > 1){
-            let ret = findStuff(targetVal - arr[i], itemCount - 1, arr.slice(i))
+            let ret = attempt2(targetVal - arr[i], itemCount - 1, arr.slice(i))
             if(ret){
                 return [arr[i], ...ret]
             }
@@ -217,8 +231,13 @@ function findStuff(targetVal, itemCount, arr){
     return null;
 }
 
-let part1_2 = findStuff(2020, 2, input.split("\n").map(i => parseInt(i))).reduce((accum, i) => accum * i, 1)
+let multReduce = (arr) => arr.reduce((accum, i) => accum * i, 1)
+
+assert.equal(multReduce(attempt2(2020, 2, testInput)), 514579)
+assert.equal(multReduce(attempt2(2020, 3, testInput)), 241861950)
+
+let part1_2 = multReduce(attempt2(2020, 2, inputLines))
 console.log("Part 1 attempt 2", part1_2);
 
-let part2 = findStuff(2020, 3, input.split("\n").map(i => parseInt(i))).reduce((accum, i) => accum * i, 1)
+let part2 = multReduce(attempt2(2020, 3, inputLines))
 console.log("Part 2", part2);
