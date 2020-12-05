2d42664bbde6d49be81e31fa05421d17c1e191e3
Stewart A <strangelytyped+github-23f9bb@gmail.com>
2020-12-05 21:28:59 +0000

    Refactor day 3 to remove intermediate arrays

--- 3.js
+++ 3.js
@@ -14,6 +14,17 @@ function countTrees(input, slope){
   return count;
 }
 
+function countTrees2(input, slope){
+  const lineLength = input.indexOf("\n") + 1;
+
+  let count = 0;
+  for(let i = 0, j = 0; (i * lineLength) < input.length; i += slope[1], j++){
+    const x = input.charAt(i * lineLength + ((j * slope[0]) % (lineLength - 1)));
+    count += (x === "#" ? 1 : 0);
+  }
+  return count;
+}
+
 const testInput = `..##.......
 #...#...#..
 .#....#..#.
@@ -26,8 +37,8 @@ const testInput = `..##.......
 #...##....#
 .#..#...#.#`;
 
-assert.equal(countTrees(testInput, [3, 1]), 7);
-console.log("Part 1", countTrees(input, [3, 1]));
+assert.equal(countTrees2(testInput, [3, 1]), 7);
+console.log("Part 1", countTrees2(input, [3, 1]));
 
 const slopes = [
   [1, 1],
@@ -37,6 +48,6 @@ const slopes = [
   [1, 2],
 ];
 
-assert.deepEqual(slopes.map(i => countTrees(testInput, i)), [2, 7, 3, 4, 2]);
-console.log("Part 2", slopes.map(i => countTrees(input, i)).reduce((accum, i) => accum * i, 1));
+assert.deepEqual(slopes.map(i => countTrees2(testInput, i)), [2, 7, 3, 4, 2]);
+console.log("Part 2", slopes.map(i => countTrees2(input, i)).reduce((accum, i) => accum * i, 1));
 
